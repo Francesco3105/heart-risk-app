7b1133a99d04068623bc601edd148621650a26fa
Francesco Mantini <francescomantiniciccio@gmail.com>
2025-09-15 14:19:22 +0200

    Form

--- pages/🩺_Simulatore_Paziente.py
+++ pages/🩺_Simulatore_Paziente.py
@@ -12,5 +12,38 @@ st.write(
     
     """
 )
-from ui import render_footer
+
+# Importa dati statici e funzioni UI
+from ui import render_field, render_footer
+from static import FEATURES, DEFAULT_PROFILE, ORDER
+
+if "patient" not in st.session_state:
+    st.session_state["patient"] = DEFAULT_PROFILE.copy()
+
+
+st.subheader("Patient — Enter your values")
+
+# New values will be collected here
+updated_values = {}
+
+# Draw each field directly (no forms/containers)
+for key in ORDER:
+   meta = FEATURES[key]
+   current_value = st.session_state["patient"].get(key, meta.get("value"))
+   new_value = render_field(key, meta, current_value)
+   updated_values[key] = new_value
+
+st.divider()
+
+
+save = st.button("Save data")
+
+
+# What happens when button is pressed
+if save:
+   st.session_state["patient"] = updated_values
+   st.success("Patient data saved. (Risk calculation comes in the next step.)")
+
+
+# Dati dell'autore
 render_footer("Francesco Mantini", "https://www.linkedin.com/in/francesco-mantini-aa8828382/", "https://github.com/Francesco3105")
